0728734addb32b74d463178f97aa5fbcd6801d57
yasser mimouni <yasmimouni2@gmail.com>
2017-11-14 15:06:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -32,3 +32,12 @@ function deepestChild()
   }
   return element;
 }
+
+function increaseRankBy(n)
+{
+  let lists = document.querySelectorAll(".ranked-list li");
+
+  for (var i = 0; i < lists.length; i++) {
+    lists[i].innerHTML = parseInt(lists[i].innerHTML) + n;
+  }
+}
