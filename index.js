7d599d05b895ca6b8ce2d80ad2f04cbfbfef3b41
yasser mimouni <yasmimouni2@gmail.com>
2017-11-14 14:25:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,3 +2,9 @@ function getFirstSelector(selector)
 {
   return document.querySelector(selector);
 }
+
+function nestedTarget()
+{
+  let element = document.getElementById("nested").querySelector(".target");
+  return element;
+}
